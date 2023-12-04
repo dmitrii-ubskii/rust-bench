e2ce183e45f83beaa53e783fb8da67d171d61feb
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2023-12-04 14:22:31 +0000

    throughput benchmarks

---
+++ btreeset-throughput/src/main.rs
@@ -0,0 +1,177 @@
+use crossbeam_skiplist::SkipSet;
+use crossbeam_utils::thread::scope;
+use rand::thread_rng;
+use rand::Rng;
+use std::sync::Mutex;
+use std::{collections::BTreeSet, time::Instant};
+
+type Key = [u8; 32];
+const KEY_SIZE: usize = std::mem::size_of::<Key>();
+
+fn main() {
+    let total_size_mb = 1024usize;
+    println!("Key size: {} bytes", KEY_SIZE);
+    println!("Total generated size: {} MiB\n", total_size_mb);
+
+    let num_keys: usize = total_size_mb * 1024 * 1024 / KEY_SIZE;
+
+    println!("# Randomly generating keys (Vec + capacity)");
+    let now = Instant::now();
+    let data: Vec<Key> = (0..num_keys).map(|_| random()).collect();
+    report_throughput(total_size_mb, now);
+
+    println!("# Vec: pushes, then sort");
+    measure(&data, |data| {
+        let mut vec = Vec::new();
+        for &x in data {
+            vec.push(x);
+        }
+        vec.sort();
+    });
+
+    println!("# Vec: pushes with_capacity, then sort");
+    measure(&data, |data| {
+        let mut vec = Vec::with_capacity(num_keys);
+        for &x in data {
+            vec.push(x);
+        }
+        vec.sort();
+    });
+
+    println!("# Vec: collect, then sort");
+    measure(&data, |data| {
+        let mut vec: Vec<Key> = std::hint::black_box(data.iter().copied().collect());
+        vec.sort();
+    });
+
+    println!("# BTreeSet: insert");
+    measure(&data, |data| {
+        let mut set = BTreeSet::new();
+        for &x in data {
+            set.insert(x);
+        }
+    });
+
+    println!("# BTreeSet: collect");
+    measure(&data, |data| {
+        let _set: BTreeSet<Key> = data.iter().copied().collect();
+    });
+
+    println!("# BTreeSet: extend");
+    measure(&data, |data| {
+        let mut set = BTreeSet::new();
+        set.extend(data.iter().copied());
+    });
+
+    println!("# BTreeSet: concurrent inserts in 4 threads");
+    measure(&data, |data| {
+        let set = Mutex::new(BTreeSet::new());
+        scope(|s| {
+            let set = &set;
+            for chunk in data.chunks(data.len() / 4) {
+                s.spawn(move |_| {
+                    for &x in chunk {
+                        set.lock().unwrap().insert(x);
+                    }
+                });
+            }
+        })
+        .unwrap();
+    });
+
+    println!("# BTreeSet: batched concurrent inserts in 4 threads");
+    measure(&data, |data| {
+        let set = Mutex::new(BTreeSet::new());
+        scope(|s| {
+            let set = &set;
+            for chunk in data.chunks(data.len() / 4) {
+                s.spawn(move |_| {
+                    for batch in chunk.chunks(32) {
+                        let mut set = set.lock().unwrap();
+                        for &x in batch {
+                            set.insert(x);
+                        }
+                    }
+                });
+            }
+        })
+        .unwrap();
+    });
+
+    println!("# SkipSet: insert");
+    measure(&data, |data| {
+        let set = SkipSet::new();
+        for &x in data {
+            set.insert(x);
+        }
+    });
+
+    println!("# SkipSet: collect");
+    measure(&data, |data| {
+        let _set: SkipSet<Key> = data.iter().copied().collect();
+    });
+
+    println!("# SkipSet: sorted inserts");
+    measure(&data, |data| {
+        let btree_set: BTreeSet<Key> = data.iter().copied().collect();
+        let set = SkipSet::new();
+        for x in btree_set {
+            set.insert(x);
+        }
+    });
+
+    println!("# SkipSet: concurrent inserts in 4 threads");
+    measure(&data, |data| {
+        let set = SkipSet::new();
+        scope(|s| {
+            let set = &set;
+            for chunk in data.chunks(data.len() / 4) {
+                s.spawn(move |_| {
+                    for &x in chunk {
+                        set.insert(x);
+                    }
+                });
+            }
+        })
+        .unwrap();
+    });
+
+    println!("# SkipSet: concurrent sorted inserts in 4 threads");
+    measure(&data, |data| {
+        let set = SkipSet::new();
+        scope(|s| {
+            let set = &set;
+            for chunk in data.chunks(data.len() / 4) {
+                s.spawn(move |_| {
+                    let btree_set: BTreeSet<Key> = chunk.iter().copied().collect();
+                    for x in btree_set {
+                        set.insert(x);
+                    }
+                });
+            }
+        })
+        .unwrap();
+    });
+}
+
+fn measure(data: &Vec<Key>, f: impl FnOnce(&Vec<Key>)) {
+    let now = Instant::now();
+    f(data);
+    report_throughput(data.len() * KEY_SIZE / 1024 / 1024, now);
+}
+
+fn report_throughput(size_mb: usize, now: Instant) {
+    let elapsed = now.elapsed();
+    println!("Done in {:.3} s", elapsed.as_secs_f64());
+    println!(
+        "Throughput: {:.2} MiB/s",
+        size_mb as f64 / elapsed.as_secs_f64()
+    );
+    println!();
+}
+
+fn random() -> Key {
+    let mut key = [0; 32];
+    thread_rng().fill(&mut key);
+    key
+}
---
+++ fs-io-throughput/src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    fs::{self, File},
+    io::Write,
+    time::{Duration, Instant},
+};
+
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let data = {
+        let mut data = vec![0u8; 1024 * 1024 * 1024];
+        thread_rng().fill(&mut *data);
+        data
+    };
+    for sync_interval in [2, 10, 50, 100].into_iter().map(Duration::from_millis) {
+        println!("Syncing all metadata every {sync_interval:?}");
+        write_benchmark(&data, sync_interval);
+    }
+}
+
+fn write_benchmark(data: &[u8], _: Duration) {
+    let mut file = File::create("./.tmp").expect("could not open ./.tmp for writing");
+    let start = Instant::now();
+    for chunk in data.chunks(4096) {
+        file.write_all(chunk)
+            .expect("could not write a chunk into ./.tmp");
+    }
+    file.sync_all().expect("could not sync file to disk");
+    drop(file);
+    report_throughput(data.len(), start);
+    fs::remove_file("./.tmp").expect("could not delete ./.tmp");
+}
+
+fn report_throughput(size: usize, now: Instant) {
+    let elapsed = now.elapsed();
+    println!("Done in {:.3} s", elapsed.as_secs_f64());
+    println!(
+        "Throughput: {:.2} MiB/s",
+        size as f64 / 1024.0 / 1024.0 / elapsed.as_secs_f64()
+    );
+    println!();
+}
